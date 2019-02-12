0d5883a2c1344068008373aa1e4f1a0421978aa0
DeltaNetworks <41640896+DeltaNetworks@users.noreply.github.com>
2019-02-12 12:38:55 +0200

    start upload

---
+++ Text_Printer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Text_Printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome !  type your message: ");
+            string msg = Console.ReadLine();
+            Prinet_Text(msg);
+            Thread.Sleep(4000);
+        }
+
+        private static void Prinet_Text(string msg)
+        {
+            Console.WriteLine("________________________________________________________________");
+            Console.WriteLine(msg);
+        }
+    }
+}
